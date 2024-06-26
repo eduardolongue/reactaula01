10c2d13e66d0a8a236c288d99b211f9ec66474c2
eduardolongue <edu.longue@gmail.com>
2024-06-26 16:31:29 -0300

    first commit

---
+++ src/Componentes/HelloWorld.jsx
@@ -0,0 +1,17 @@
+import './style.css';
+
+function HelloWorld({conteudo}) {
+    return (
+        <h1 className="teste">Olá {conteudo}</h1>
+    );
+}
+
+function cabecalho(){
+    return(
+        <header>
+            <h1>asdadas</h1>
+        </header>
+    );
+}
+
+export default HelloWorld;
